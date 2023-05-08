741315bab6b257826230e9b0b7e7a29a2ac48a33
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-05-08 19:00:07 +0800

    Create firebase-method-2.js

---
+++ firebase-method-2.js
@@ -0,0 +1,16 @@
+const firebase = require('firebase');
+
+const config = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+
