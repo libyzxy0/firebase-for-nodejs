80a2e78ffbce08bd94a6566952f999d3309dfbcf
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-05-08 19:01:15 +0800

    Update firebase-method-2.js

--- firebase-method-2.js
+++ firebase-method-2.js
@@ -11,6 +11,57 @@ const config = {
 
 firebase.initializeApp(config);
 
-const database = firebase.database();
+const db = firebase.database();
 
+function writeData(path, obj, callback) {
+  const ref = db.ref(path);
+  ref.push(obj)
+  .then((snapshot) => {
+    callback({ msg: "success" });
+  })
+  .catch((error) => {
+    callback({ msg: "error" });
+  });
+}
 
+function readData(path, callback) {
+  const ref = db.ref(path);
+  ref.once('value', (snapshot) => {
+  const data = snapshot.val();
+  if (data) {
+    let dataArray = [];
+    snapshot.forEach((childSnapshot) => {
+      const childData = childSnapshot.val();
+      delete childData.id;
+      dataArray.push(childData)
+    });
+    callback(dataArray);
+  }
+}).catch((error) => {
+  callback({ msg: "error" });
+});
+}
+
+function updateData(path, obj, callback) {
+  const ref = db.ref(path);
+  ref.update(obj)
+  .then(() => {
+    callback({ msg: "success" });
+  })
+  .catch((error) => {
+    callback({ msg: "error" });
+  });
+}  
+
+function removeData(path, callback) {
+  const ref = db.ref(path);
+  ref.remove()
+  .then(() => {
+    callback({ msg: "success" });
+  })
+  .catch((error) => {
+    callback({ msg: "error" });
+  });
+}
+
+module.exports = { writeData, readData, updateData, removeData } 
