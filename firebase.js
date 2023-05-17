26753b634ee347685effe09f4d19682642fe4c98
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-05-18 07:51:26 +0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,63 +1,84 @@
 const admin = require('firebase-admin');
-
 const sA = require('./serviceAccountKey.json');
-
 admin.initializeApp({
-  credential: admin.credential.cert(sA),
-  databaseURL: process.env.DB_URL
+	credential: admin.credential.cert(sA),
+	databaseURL: process.env.DB_URL
 });
-
 const db = admin.database();
 
-function writeData(path, obj, callback) {
-  const ref = db.ref(path);
-  ref.push(obj)
-  .then((snapshot) => {
-    callback({ msg: "success", method: "write" });
-  })
-  .catch((error) => {
-    callback({ msg: "error", method: "write"});
-  });
+function writeData(path, obj) {
+	return new Promise((resolve, reject) => {
+		const ref = db.ref(path);
+		ref.push(obj).then((snapshot) => {
+			resolve({
+				msg: "success",
+				method: "write"
+			});
+		}).catch((error) => {
+			reject({
+				msg: "error",
+				method: "write"
+			});
+		});
+	});
 }
 
-function readData(path, callback) {
-  const ref = db.ref(path);
-  ref.once('value', (snapshot) => {
-  const data = snapshot.val();
-  if (data) {
-    let dataArray = [];
-    snapshot.forEach((childSnapshot) => {
-      const childData = childSnapshot.val();
-      childData["fid"] = childSnapshot.key;
-      dataArray.push(childData)
-    });
-    callback(dataArray);
-  }
-}).catch((error) => {
-  callback({ msg: "error", method: "read"});
-});
+function readData(path) {
+	return new Promise((resolve, reject) => {
+		const ref = db.ref(path);
+		ref.once('value', (snapshot) => {
+			const data = snapshot.val();
+			if(data) {
+				let dataArray = [];
+				snapshot.forEach((childSnapshot) => {
+					const childData = childSnapshot.val();
+					childData["fid"] = childSnapshot.key;
+					dataArray.push(childData)
+				});
+				resolve(dataArray);
+			}
+		}).catch((error) => {
+			reject({
+				msg: "error",
+				method: "read"
+			});
+		});
+	});
 }
 
-function updateData(path, obj, callback) {
-  const ref = db.ref(path);
-  ref.update(obj)
-  .then(() => {
-    callback({ msg: "success", method: "update"});
-  })
-  .catch((error) => {
-    callback({ msg: "error", method: "update"});
-  });
-}  
-
-function removeData(path, callback) {
-  const ref = db.ref(path);
-  ref.remove()
-  .then(() => {
-    callback({ msg: "success", method: "remove" });
-  })
-  .catch((error) => {
-    callback({ msg: "error", method: "remove" });
-  });
+function updateData(path, obj) {
+	return new Promise((resolve, reject) => {
+		const ref = db.ref(path);
+		ref.update(obj).then(() => {
+			resolve({
+				msg: "success",
+				method: "update"
+			});
+		}).catch((error) => {
+			reject({
+				msg: "error",
+				method: "update"
+			});
+		});
+	});
 }
 
-module.exports = { writeData, readData, updateData, removeData } 
+function removeData(path) {
+	return new Promise((resolve, reject) => {
+		const ref = db.ref(path);
+		ref.remove().then(() => {
+			callback({
+				msg: "success",
+				method: "remove"
+			});
+		}).catch((error) => {
+			callback({
+				msg: "error",
+				method: "remove"
+			});
+		});
+	});
+}
+module.exports = {
+	writeData, readData, updateData, removeData
+}
